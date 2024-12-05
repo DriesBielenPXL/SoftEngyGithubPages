ebe130030cebebdd2b914e15a59e98999cb8e19b
DriesBielenPXL <dries.bielen10@hotmail.be>
2024-12-05 07:43:58 +0000

    Automated Release $(Release.ReleaseId)

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"a18df97ca57a249df5d8d68cd0820600223ce2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "688324498"
+    serviceWorkerVersion: "2524345796"
   }
 });
